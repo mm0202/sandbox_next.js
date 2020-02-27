78857a0ec1f9ff9e52463ecd0447b7496ddbe56a
m <for_etc@hotmail.com>
2020-02-27 11:32:40 +0900

    Headerコンポーネント追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,31 @@
 import React from "react";
 import Link from 'next/link'
+import Header from "../components/Header";
 
 const Index = () => (
-    <ul>
-        <li>
-            <p>Hello Next.js</p>
-        </li>
-        <li>
-            <Link href={"/about"}>
-                <a>About Page</a>
-            </Link>
-        </li>
-        <li>
-            <Link href={"/about"}>
-                <a style={{fontSize: 20}}>About Page</a>
-            </Link>
-        </li>
-        <li>
-            <Link href={"/about"}>
-                <button>Go to About Page</button>
-            </Link>
-        </li>
-    </ul>
+    <div>
+        <Header/>
+        <ul>
+            <li>
+                <p>Hello Next.js</p>
+            </li>
+            <li>
+                <Link href={"/about"}>
+                    <a>About Page</a>
+                </Link>
+            </li>
+            <li>
+                <Link href={"/about"}>
+                    <a style={{fontSize: 20}}>About Page</a>
+                </Link>
+            </li>
+            <li>
+                <Link href={"/about"}>
+                    <button>Go to About Page</button>
+                </Link>
+            </li>
+        </ul>
+    </div>
 );
 
 export default Index
