1c0a5504613c167651e8e9b6d18f94f059244c78
m <for_etc@hotmail.com>
2020-02-27 11:22:15 +0900

    tune

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,21 @@ import React from "react";
 import Link from 'next/link'
 
 const Index = () => (
-    <div>
-        <Link href={"/about"}>
-            <a style={{fontSize: 20}}>About Page</a>
-        </Link>
-        <p>Hello Next.js</p>
-    </div>
+    <ul>
+        <li>
+            <Link href={"/about"}>
+                <a>About Page</a>
+            </Link>
+        </li>
+        <li>
+            <Link href={"/about"}>
+                <a style={{fontSize: 20}}>About Page</a>
+            </Link>
+        </li>
+        <li>
+            <p>Hello Next.js</p>
+        </li>
+    </ul>
 );
 
 export default Index
